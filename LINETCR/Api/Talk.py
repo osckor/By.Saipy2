a720462cbb313ea13b6ad6cc670b5e455ddc83ec
test <35461512+Downack@users.noreply.github.com>
2018-03-03 19:58:34 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.4.17"
-  LA = "CHROMEOS\t1.4.17\tChrome_OS\t1"
+  UA = "Line/8.4.2"
+  LA = "CHROMEOS\t8.4.2\tChrome_OS\t1"
 
   authToken = None
   cert = None
@@ -89,7 +89,7 @@ class Talk:
   def qrLogin(self, callback):
     self.transport.path = self.auth_query_path
 
-    qr = self.client.getAuthQrcode(True, "PRANKBOTS")
+    qr = self.client.getAuthQrcode(True, "Sai bot")
     callback("Copy to Line and Click\nYour LINK QR is: line://au/q/" + qr.verifier)
 
     r = requests.get("https://" + self.host + self.wait_for_mobile_path, headers={
